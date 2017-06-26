e8cdef63cfba7775f5e0eef0296ed1c7e59d8d6a
Eugene Lynch <eugene@enspiral.com>
2017-06-26 08:20:01 -0400

    beginning of home page

--- src/pages/home.js
+++ src/pages/home.js
@@ -2,7 +2,14 @@ const html = require('choo/html')
 
 const home = (state, emit) => {
   return html`
-    <h1>eugene lynch</h1>
+    <div id='home'>
+      <h1>eugene lynch</h1>
+      <ul>
+        <li class='list-item'><span>million dollar startup ideas</span></li>
+        <li class='list-item'><span>parallel universes</span></li>
+        <li class='list-item'><span>professional cat photography</span></li>
+      </ul>
+    </div>
   `
 }
 
